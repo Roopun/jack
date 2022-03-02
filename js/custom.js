d1be7e3203653e1b492077407d78acd0f8a19a09
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2022-03-02 17:36:42 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+
+    $('ul.nav li').on('click', function () {
+        
+        $(this).parent().find('li.active').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+    //    //smooth scroll and active menu button code
+    //    var html_body = $('html, body');
+    //    $('.navbar .navbar-nav .nav-item .nav-link').on('click', function () {
+    //        //for active menu button
+    //        $(this).addClass('active').siblings().removeClass('active');
+    //        //smooth scroll on menu click
+    //        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    //            var target = $(this.hash);
+    //            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //            if (target.length) {
+    //                html_body.animate({
+    //                    scrollTop: target.offset().top - 0
+    //                }, 1000);
+    //                return false;
+    //            }
+    //        }
+    //    });
+
+
+
+    //    navbar code
+
+    $(window).scroll(function () {
+
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > 350) {
+
+            $(".navbar").addClass("active-navbar");
+        } else {
+
+            $(".navbar").removeClass("active-navbar");
+        }
+
+    });
+
+    //    jackpot type
+
+    var typed = new Typed('#jackpot-type', {
+        // Waits 1000ms after typing "First"
+        strings: ['oday’s Jackpot', 'oday’s Jackpot'],
+        typeSpeed: 300,
+        loop: true,
+        backSpeed: 300,
+        smartBackspace: true,
+    });
+
+
+
+
+
+
+});
